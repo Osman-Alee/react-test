8741099c961d6a5ca42438d16a917f65b2c84bc9
Osman-Alee <merasohna000@gmail.com>
2022-06-08 15:13:38 +0500

    tooq

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Please do not click this button.
         </a>
       </header>
     </div>
